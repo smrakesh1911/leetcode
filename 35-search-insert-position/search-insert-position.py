316e6aa1d126e5fcc5bb4c647caa6a5150e2bf91
smrakesh1911 <rakeshsm2004@gmail.com>
2026-09-18 12:03:23 +0530

    Time: 0 ms (100.00%) | Memory: 20 MB (44.62%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.py
@@ -0,0 +1,17 @@
+class Solution:
+    def lowerBound(self,nums,target):
+        n = len(nums)
+        l = 0
+        r = n-1
+        ans = n
+        while l <= r:
+            mid=(l+r)//2
+            if nums[mid] >= target:
+                ans = mid
+                r = mid-1
+            else:
+                #right
+                l =mid+1
+        return ans
+    def searchInsert(self, nums: list[int], target: int) -> int:
+         return self.lowerBound(nums,target)
